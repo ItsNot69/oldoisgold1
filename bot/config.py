9d639c18d7bb73e03e875bfc71e04cc463d9239c
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-07-14 04:58:25 -0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ class Config(object):
     CHAT_ID = [int(x) for x in os.environ.get(
         "CHAT_ID", "-1001892721748").split()]
     SESSION_STRING = os.environ.get("SESSION_STRING", "BQGtHfYAec7-xLDFRHpMbWcuTc0laakaE4goiCLLhQLlRBJ3R596G6cbdRxRXmusvdr4YsS0czQ9FfI3RfcoRO98qoDuhdakuVwEuBNpZTlO9epsFkFBjpmS2d4z4nvuFVz2ES3xURZIcixqdmCznHzKKnWo47eYhXQ_0JEWwbAZoTJoHBJNA7RbpYZ0Pcrv1vxPuJGRuVmiPi397LkqzmoIqCr6c1PBcmHDVxi1UxSYaEpv9uopW2trH6Lh5IP-pTuhZQ35M5oZDHe8QHMiCm3s5wt9dInJf73vP50tLm4S5CkpRmjLrITM-l2HM1lt5s6iDh5BHG27lggbyW3WCqn7LHlJjgAAAAFZz5IaAA")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://m0921594:8A2mcGVTT3eDnkBk@cluster0.i7dsnm5.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://m0921594:Rohit44@cluster0.d1sg6dv.mongodb.net/?retryWrites=true&w=majority")
     MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "Telegram")
 
 
