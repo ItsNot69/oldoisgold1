9070a016b8b5f2774a93254b7f8640f2c5714989
ItsNot69 <126782133+ItsNot69@users.noreply.github.com>
2025-01-17 19:39:04 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -14,8 +14,8 @@ def is_enabled(value, default):
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "28726816"))
-    API_HASH = os.environ.get("API_HASH", "45edf74203ecf6ff14b394a9e47dca34")
+    API_ID = int(os.environ.get("API_ID", "21787593"))
+    API_HASH = os.environ.get("API_HASH", "0a5c5e27ce46ad36e19eb91c02affb99")
     BOT_TOKEN = os.environ.get(
         "BOT_TOKEN", "6059695462:AAHHGQk0mQj-Q06Yt7_8yoiBejI-HwCN90k")
     OWNER_ID = int(os.environ.get("OWNER_ID", "5521380948"))
@@ -23,7 +23,7 @@ class Config(object):
     CHAT_ID = [int(x) for x in os.environ.get(
         "CHAT_ID", "-1001654163482").split()]
     SESSION_STRING = os.environ.get("SESSION_STRING", "BQGB5kgAYuzF4QGIJ15KM1gDLdzj6bO--7Du3AZb1geB8GQ_O7Dow-WQgCWGvAoleUMpnX52bXGYO4MrrJCxsZlfrVYwFMnRBfE2l_CgqaB4fDO7rtww5DKFSdT0zBSOzIVACdfv7EhdfyGzqo74QCiVdWorrv91p9TItuzBeO3KRh-hz5rCQ5XxgIj3v9ms6VRCnJVo3jhGz4gZUE8lHFJeMr8qeve1Uo0twzgGkmHjqlliLvro5FyiRdfFRwLs5xT53Vv61L1Be0umt5w7Wkeq5KXP-BSjXHGnFtg10qcZdheTtcu7a3qrwG8ki7YI9OgHhAslUbLKj4ByuUz-izt1UQVTLQAAAAF76MikAA")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://m0921594:Rohit11@cluster0.i7dsnm5.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Rohit33:Rohit33@cluster0.i7dsnm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "Telegram")
 
 
